056233ec0a19186b62ee443db794f8fb368bc177
Arnaldo Escossio <arnaldoescossio@gmail.com>
2014-04-11 17:35:35 -0300

    Inicio do projeto

---
+++ app/models/user.rb
@@ -0,0 +1,25 @@
+class User < ActiveRecord::Base
+  EMAIL_REGEXP = /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
+
+  validates_presence_of :email, :full_name, :location, :password
+  validates_confirmation_of :password
+  validates_length_of :bio, minimum: 30, allow_blank: false
+  validates_uniqueness_of :email
+
+  # Essa validação pode ser representada da seguinte forma:
+  # validates_format_of :email, with: EMAIL_REGEXP
+  validate do
+    errors.add(:email, :invalid) unless :email.match(EMAIL_REGEXP)
+  end
+
+=begin
+  validates :email, presence: true,
+            format: { with: /\A[^@]+@[^@\.]+\z/ },
+            uniqueness: true
+
+  Ainda é possível colocar múltiplos atributos na validação:
+  validates :email, :full_name, :location, presence: true
+=end
+
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Colchonet::Application.config.secret_key_base = '2a06d38f3bf8cad3dd1e2ed654e514fcaa13f5e6b4456e70e509c0d798e1d9341df429d32b84383c26d76a6662a95281a0256d8feddf5053c698c11a6968b9fc'
